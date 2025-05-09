6e14954f294ae9272ddd5ec188ebd22d3a7ebe10
edamhr <edam_hr@naver.com>
2025-05-09 16:08:35 +0900

    Update video-control.js

--- video-control.js
+++ video-control.js
@@ -1,22 +1,14 @@
 const video = document.getElementById("lectureVideo");
-const progressBar = document.getElementById("progressBar");
-const progressText = document.getElementById("progressText");
 
 video.addEventListener("loadedmetadata", function () {
   video.maxWatched = 0;
 });
 
 video.addEventListener("timeupdate", function () {
-  // 진도율 계산
-  const percent = ((video.currentTime / video.duration) * 100).toFixed(1);
-  progressBar.style.width = percent + "%";
-  progressText.textContent = "진도율: " + percent + "%";
-
-  // 건너뛰기 방지
+  // 사용자가 이미 본 구간까지만 재생 허용
   if (video.currentTime > video.maxWatched + 0.5) {
     video.currentTime = video.maxWatched;
   } else {
     video.maxWatched = video.currentTime;
   }
 });
-
